ae4dfa1d732c18abc8598696d06509e55ec1947f
Luis Roberto Perez <luis.robertop87@gmail.com>
2015-10-30 08:31:13 -0400

    Avoid Collections.sort() from Solver solution, decrease execution time

--- MissileZombies/src/missilezombies/Solver.java
+++ MissileZombies/src/missilezombies/Solver.java
@@ -1,43 +1,39 @@
 package missilezombies;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 /**
  *
  * @author Luis Roberto <luis.robertop87@gmail.com>
  */
 public class Solver {
-    
+
     public static int MISSILES = 3;
-    
+
     public static String solution(Grid grid) {
-        
+
         String output = "";
         //Check all point to attack and get its AttackResult
         for (int missile = 0; missile < MISSILES; missile++) {
-            List<AttackResult> results = new ArrayList<>();
+            int maxZombiesAttacked = Integer.MIN_VALUE;
+            AttackResult result = null;
+            AttackResult bestShoot = null;
+
             for (int x = 1; x <= grid.dimension.width; x++) {
                 for (int y = 0; y <= grid.dimension.height; y++) {
-                    AttackResult result = grid.receiveImpactWith(new Missile(x, y, grid), false);
+                    result = grid.receiveImpactWith(new Missile(x, y, grid), false);
                     if (result.zombies > 0) {
-                        results.add(result);
+                        if (result.zombies >= maxZombiesAttacked) {
+                            maxZombiesAttacked = result.zombies;
+                            bestShoot = result;
+                        }
                     }
                 }
             }
 
-            Collections.sort(results);
-
-            try {
-                output += String.format("%s\n", results.get(missile));
-                grid.receiveImpactWith(results.get(missile).missile, true);
-            } catch (IndexOutOfBoundsException ioe) {
-                break;
-            }
+            output += String.format("%s\n", bestShoot);
+            grid.receiveImpactWith(bestShoot.missile, true);
         }
-        
+
         return output;
     }
-    
+
 }
